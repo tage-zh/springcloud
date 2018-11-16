e44a0a86a2080f5958354d3a620e28978c6f799f
tiger <117676896@qq.com>
2018-11-16 23:28:52 +0800

    docker

--- user-service/src/main/java/com/tiger/userservice/service/UserService.java
+++ user-service/src/main/java/com/tiger/userservice/service/UserService.java
@@ -1,15 +1,18 @@
 package com.tiger.userservice.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserService {
-
+    private Log log = LogFactory.getLog(this.getClass());
     @Value("${server.port}")
     private String port;
 
     public String sayHello(){
+        log.info("start hello");
         return "Hello from "+ port;
     }
 }
