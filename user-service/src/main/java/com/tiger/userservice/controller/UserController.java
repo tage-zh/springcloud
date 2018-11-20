434744b37232dc9d91bacb280e284b4ec6e2ec6f
tiger <117676896@qq.com>
2018-11-20 18:21:37 +0800

    config

--- user-service/src/main/java/com/tiger/userservice/controller/UserController.java
+++ user-service/src/main/java/com/tiger/userservice/controller/UserController.java
@@ -3,7 +3,9 @@ package com.tiger.userservice.controller;
 import com.tiger.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +18,10 @@ public class UserController {
     @RequestMapping("/hello")
     public String hello(){
         return  userService.sayHello();
+    }
+
+    @RequestMapping("/webhook")
+    public void webhook(@RequestBody Object payLoad){
+        System.out.println(payLoad);
     }
 }
